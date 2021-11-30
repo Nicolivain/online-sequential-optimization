09de83f3f81025cb22d0a02adcf7e77b919c43be
Nicolas Olivain <nicolasolivain@live.com>
2021-11-30 23:33:22 +0100

    projector on L1 Project added projection on L1

---
+++ Algorithms/Projector.py
@@ -0,0 +1,34 @@
+import numpy as np
+from copy import deepcopy
+
+
+def proj_l1(vect):
+    """
+    Projects the vector vect on the L1-ball
+    :param vect: vector of size (n)
+    """
+
+    if sum([abs(vi) for vi in vect]) == 1:
+        return vect
+
+    nvc  = list(deepcopy(vect))
+    topk = [nvc.pop(np.argmax(vect))]
+    d0 = 1
+    while nvc[np.argmax(nvc)] > 1/d0 * (sum(topk) - 1):
+        topk.append(nvc.pop(np.argmax(nvc)))
+        d0 += 1
+        if d0 > len(vect):
+            break
+
+    theta = 1/d0 * (sum(topk) - 1)
+
+    soft_threshold = [max(vi-theta, 0) for vi in vect]
+    return soft_threshold
+
+
+if __name__ == '__main__':
+    vect = np.random.rand(10)
+    p = proj_l1(vect)
+    print(vect)
+    print(p)
+    print(sum([abs(vi) for vi in p]))

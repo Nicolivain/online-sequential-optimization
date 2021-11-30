fd8380fe744d23166cbf88324aec22f888876cce
Nicolas Olivain <nicolasolivain@live.com>
2021-11-30 23:06:52 +0100

    SGD + cleaning

--- Algorithms/GD.py
+++ Algorithms/GD.py
@@ -1,24 +1,24 @@
 """
 This file contains functions for gradient descent algorithm applied at the SVM problem
 """
-import numpy as np
-from utils import LinearSVM
+
 
 def GradientDescent(model, X, y, lr, epoch, l, verbose=0):
     """
-    Gradient descent algorithms applied with the CO pb il loss and uses tjhe gradloss function to update parameters
-    X (nxm) : data
-    y (n) : labels 
-    lr (float) : learning rate
-    epoch (int) : maximum number of iteration of the algorithm
-    l (float) : regularization parameter (lambda)"""
-    n, m = X.shape
-    params = np.random.rand(m)
+    Unconstrained GD
+    :param X: (nxm) data
+    :param y: (n)  labels
+    :param lr: (float) learning rate
+    :param epoch: (int) maximum number of iteration of the algorithm
+    :param l:  (float) regularization parameter (lambda)
+    :param verbose: (int) print epoch results every n epochs
+    """
     losses = []
     for i in range(epoch):
-        params -= lr * model.gradLoss(params, X, y, l)
-        current_loss = model.loss(params, X, y, l)
+        model.w -= lr * model.gradLoss(X, y, l)
+        current_loss = model.loss(X, y, l)
         losses += [current_loss]
-        if verbose > 0  and i % verbose == 0:
-            print(f"epoch {i} : loss = {current_loss}")
-    return params, losses
+        if verbose > 0 and i % verbose == 0:
+            print("Epoch {:3d} : Loss = {:1.4f}".format(i, current_loss))
+    return losses
+
--- utils.py
+++ utils.py
@@ -1,49 +1,31 @@
-"""
-file with usefull functions
-"""
-
 import numpy as np
+import matplotlib.pyplot as plt
 
-class LinearSVM():
-    def __init__(self) -> None:
-        pass
 
-    def accuracy(self, y_pred, y_true):
-        """
-        Compute the accuracy of the provided predictions
-        y_pred (n) : prediciton
-        y_true (n) : true value to predict
-        """
-        if len(y_pred) != len(y_true):
-            raise KeyError("prediction and truth must be the same size")
-        return(np.sum(y_pred==y_true)/len(y_true))
+def accuracy(y_pred, y_true):
+    """
+    Compute the accuracy of the provided predictions
+    y_pred (n) : prediciton
+    y_true (n) : true value to predict
+    """
+    if len(y_pred) != len(y_true):
+        raise KeyError("prediction and truth must be the same size")
+    return np.sum(y_pred == y_true)/len(y_true)
 
-    def loss(self, x, a, b, l):
-        """
-        Compute the loss for the data and provided parameters
-        x (m) : params 
-        a (nxm) : data
-        b (n) : labels
-        l (float ): regularization parameter (lambda)"""
-        temp = 1 - np.dot(a, x) * b
-        temp[np.where(temp < 0)] = 0
-        return np.mean(temp) + l/2*np.linalg.norm(x)
-        
-    def gradLoss(self, x, a, b, l, ):
-        """
-        Compute the gradient of the loss wrt the current parameters and data
-        x (m) : params 
-        a (nxm) : data
-        b (n) : labels
-        l (float) : regularization parameter (lambda)"""
-        temp = 1 - np.dot(a, x)
-        grad = - (np.repeat(b[:, np.newaxis], a.shape[1], 1)) * a # reshape b to nxm to use term-by-term multiplication
-        grad[np.where(temp < 0)] = 0
-        return np.sum(grad, 0) + l * x
 
-    def predict(self, x, data):
-        """
-        predict values using coeff x and new data data
-        /!\ return values in -1/1 format instead of -1/0 as labels
-        """
-        return -1 * (data.dot(x) < 0)
+def plot_loss(loss, graph_title=None):
+    """
+    a useful function to plot curves
+    :param loss: list (n) or dict (key, list(n)) values or batch of values to be plotted
+    :param graph_title: (str) grah
+    """
+
+    if type(loss) == list:
+        idx = [i for i in range(len(loss))]
+        return plt.plot(idx, loss, title=graph_title)
+    else:
+        ax = None
+        for key, vals in loss.items():
+            idx = [i for i in range(len(vals))]
+            ax = plt.plot(idx, vals, title=graph_title, legend=key)
+        return ax

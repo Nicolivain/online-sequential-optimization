6b5406b2184eeab30b16c492e7a8c2488cb6325c
2ailesB <leboudec.lise@gmail.com>
2021-12-26 21:34:12 +0100

    fix/adam losses + formatting

--- Algorithms/Adam.py
+++ Algorithms/Adam.py
@@ -7,7 +7,7 @@ import numpy as np
 from Algorithms.Projector import proj_l1, weighted_proj_simplex
 
 
-def adam(model, X, y, lr, epoch, l, z=1, betas = [0.9, 0.999], verbose=0):
+def adam(model, X, y, lr, epoch, l, z=1, betas=[0.9, 0.999], verbose=0):
     """
     Gradient descent algorithms applied with the CO pb il loss and uses tjhe gradloss function to update parameters
     :param model: the model
@@ -39,8 +39,10 @@ def adam(model, X, y, lr, epoch, l, z=1, betas = [0.9, 0.999], verbose=0):
         # update the last xt
         t = i + 1
 
-        mts = betas[0] * mt_1s + (1 - betas[0]) * model.gradLoss(sample_x, sample_y, l)
-        vts = betas[1] * vt_1s + (1 - betas[1]) * model.gradLoss(sample_x, sample_y, l)**2
+        mts = betas[0] * mt_1s + (1 - betas[0]) * \
+            model.gradLoss(sample_x, sample_y, l)
+        vts = betas[1] * vt_1s + (1 - betas[1]) * \
+            model.gradLoss(sample_x, sample_y, l)**2
         mtchap = mts/(1 - betas[0]**t)
         vtchap = vts/(1 - betas[1]**t)
 
@@ -61,7 +63,7 @@ def adam(model, X, y, lr, epoch, l, z=1, betas = [0.9, 0.999], verbose=0):
     return losses, np.array(wts)
 
 
-def adamP(model, X, y, lr, epoch, l, z=1, betas = [0.9, 0.999], p=2, verbose=0):
+def adamP(model, X, y, lr, epoch, l, z=1, betas=[0.9, 0.999], p=2, verbose=0):
     """
     Gradient descent algorithms applied with the CO pb il loss and uses tjhe gradloss function to update parameters
     :param model: the model
@@ -94,8 +96,10 @@ def adamP(model, X, y, lr, epoch, l, z=1, betas = [0.9, 0.999], p=2, verbose=0):
         # update the last xt
         t = i + 1
 
-        mts = betas[0] * mt_1s + (1 - betas[0]) * model.gradLoss(sample_x, sample_y, l)
-        vts = betas[1]**p * vt_1s + (1 - betas[1]**p) * np.abs(model.gradLoss(sample_x, sample_y, l))**p
+        mts = betas[0] * mt_1s + (1 - betas[0]) * \
+            model.gradLoss(sample_x, sample_y, l)
+        vts = betas[1]**p * vt_1s + (1 - betas[1]**p) * \
+            np.abs(model.gradLoss(sample_x, sample_y, l))**p
         mtchap = mts/(1 - betas[0]**t)
         vtchap = vts/(1 - betas[1]**t)
 
@@ -116,7 +120,7 @@ def adamP(model, X, y, lr, epoch, l, z=1, betas = [0.9, 0.999], p=2, verbose=0):
     return losses, np.array(wts)
 
 
-def adamproj(model, X, y, lr, epoch, l, z=1, betas = [0.9, 0.999], verbose=0):
+def adamproj(model, X, y, lr, epoch, l, z=1, betas=[0.9, 0.999], verbose=0):
     """
     Gradient descent algorithms applied with the CO pb il loss and uses tjhe gradloss function to update parameters
     :param model: the model
@@ -148,13 +152,15 @@ def adamproj(model, X, y, lr, epoch, l, z=1, betas = [0.9, 0.999], verbose=0):
         # update the last xt
         t = i + 1
 
-        mts = betas[0] * mt_1s + (1 - betas[0]) * model.gradLoss(sample_x, sample_y, l)
-        vts = betas[1] * vt_1s + (1 - betas[1]) * model.gradLoss(sample_x, sample_y, l)**2
+        mts = betas[0] * mt_1s + (1 - betas[0]) * \
+            model.gradLoss(sample_x, sample_y, l)
+        vts = betas[1] * vt_1s + (1 - betas[1]) * \
+            model.gradLoss(sample_x, sample_y, l)**2
         mtchap = mts/(1 - betas[0]**t)
         vtchap = vts/(1 - betas[1]**t)
 
         new_wts = wts[-1] - lr * mtchap / (np.sqrt(vtchap + 10e-8))
-        new_wts  = weighted_proj_simplex(new_wts, np.diag(vts))
+        new_wts = weighted_proj_simplex(new_wts, np.diag(vts))
         wts.append(new_wts)
         model.w = new_wts
 
@@ -171,7 +177,7 @@ def adamproj(model, X, y, lr, epoch, l, z=1, betas = [0.9, 0.999], verbose=0):
     return losses, np.array(wts)
 
 
-def adaMax(model, X, y, lr, epoch, l, z=1, betas = [0.9, 0.999], verbose=0):
+def adaMax(model, X, y, lr, epoch, l, z=1, betas=[0.9, 0.999], verbose=0):
     """
     Gradient descent algorithms applied with the CO pb il loss and uses tjhe gradloss function to update parameters
     :param model: the model
@@ -203,10 +209,12 @@ def adaMax(model, X, y, lr, epoch, l, z=1, betas = [0.9, 0.999], verbose=0):
         # update the last xt
         t = i + 1
 
-        mts = betas[0] * mt_1s + (1 - betas[0]) * model.gradLoss(sample_x, sample_y, l)
-        vts = np.maximum(betas[1] * vt_1s, np.abs(model.gradLoss(sample_x, sample_y, l)))
+        mts = betas[0] * mt_1s + (1 - betas[0]) * \
+            model.gradLoss(sample_x, sample_y, l)
+        vts = np.maximum(betas[1] * vt_1s,
+                         np.abs(model.gradLoss(sample_x, sample_y, l)))
 
-        new_wts = wts[-1] - lr /(1 - betas[0]**t) * mts / vts
+        new_wts = wts[-1] - lr / (1 - betas[0]**t) * mts / vts
         wts.append(new_wts)
         model.w = new_wts
 
@@ -223,7 +231,7 @@ def adaMax(model, X, y, lr, epoch, l, z=1, betas = [0.9, 0.999], verbose=0):
     return losses, np.array(wts)
 
 
-def adamTemporal(model, X, y, lr, epoch, l, z=1, betas = [0.9, 0.999], verbose=0):
+def adamTemporal(model, X, y, lr, epoch, l, z=1, betas=[0.9, 0.999], verbose=0):
     """
     Gradient descent algorithms applied with the CO pb il loss and uses tjhe gradloss function to update parameters
     :param model: the model
@@ -257,13 +265,16 @@ def adamTemporal(model, X, y, lr, epoch, l, z=1, betas = [0.9, 0.999], verbose=0
         # update the last xt
         t = i + 1
 
-        mts = betas[0] * mt_1s + (1 - betas[0]) * model.gradLoss(sample_x, sample_y, l)
-        vts = betas[1] * vt_1s + (1 - betas[1]) * model.gradLoss(sample_x, sample_y, l)**2
+        mts = betas[0] * mt_1s + (1 - betas[0]) * \
+            model.gradLoss(sample_x, sample_y, l)
+        vts = betas[1] * vt_1s + (1 - betas[1]) * \
+            model.gradLoss(sample_x, sample_y, l)**2
         mtchap = mts/(1 - betas[0]**t)
         vtchap = vts/(1 - betas[1]**t)
 
-        tetats = wts[-1] - lr * mtchap / (np.sqrt(vtchap) + 10e-8)
-        new_wts = (betas[1] * tetat_1s + (1 - betas[1]) * tetats) / (1 - betas[1]**t)
+        tetats = tetat_1s - lr * mtchap / (np.sqrt(vtchap) + 10e-8)
+        new_wts = (betas[1] * tetat_1s + (1 - betas[1])
+                   * tetats) / (1 - betas[1]**t)
 
         wts.append(new_wts)
         model.w = new_wts
@@ -271,7 +282,7 @@ def adamTemporal(model, X, y, lr, epoch, l, z=1, betas = [0.9, 0.999], verbose=0
         # loss
         current_loss = model.loss(X, y, l)
         losses.append(current_loss)
-        
+
         mt_1s = mts
         vt_1s = vts
         tetat_1s = tetats
@@ -282,7 +293,7 @@ def adamTemporal(model, X, y, lr, epoch, l, z=1, betas = [0.9, 0.999], verbose=0
     return losses, np.array(wts)
 
 
-def adaMaxTemporal(model, X, y, lr, epoch, l, z=1, betas = [0.9, 0.999], verbose=0):
+def adaMaxTemporal(model, X, y, lr, epoch, l, z=1, betas=[0.9, 0.999], verbose=0):
     """
     Gradient descent algorithms applied with the CO pb il loss and uses tjhe gradloss function to update parameters
     :param model: the model
@@ -316,11 +327,14 @@ def adaMaxTemporal(model, X, y, lr, epoch, l, z=1, betas = [0.9, 0.999], verbose
         # update the last xt
         t = i + 1
 
-        mts = betas[0] * mt_1s + (1 - betas[0]) * model.gradLoss(sample_x, sample_y, l)
-        vts = np.maximum(betas[1] * vt_1s, np.abs(model.gradLoss(sample_x, sample_y, l)))
+        mts = betas[0] * mt_1s + (1 - betas[0]) * \
+            model.gradLoss(sample_x, sample_y, l)
+        vts = np.maximum(betas[1] * vt_1s,
+                         np.abs(model.gradLoss(sample_x, sample_y, l)))
 
-        tetats = wts[-1] - lr / (1 - betas[0]**t) * mts / vts
-        new_wts = (betas[1] * tetat_1s + (1 - betas[1]) * tetats) / (1 - betas[1]**t)
+        tetats = tetat_1s - lr / (1 - betas[0]**t) * mts / vts
+        new_wts = (betas[1] * tetat_1s + (1 - betas[1])
+                   * tetats) / (1 - betas[1]**t)
 
         wts.append(new_wts)
         model.w = new_wts
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Liautaud Paul
 Le Boudec Lise
 
 Main file 
-""" 
+"""
 
 import numpy as np
 import pandas as pd
@@ -24,25 +24,30 @@ from utils import *
 
 # --- PARAMETERS ---
 
-lr          = 0.1
-nepoch      = 100
-lbd         = 1
-z           = 10
-verbose     = 1
+lr = 0.1
+nepoch = 100
+lbd = 1
+z = 10
+verbose = 1
 
-alg_to_run = ['gd', 'c_gd', 'sgd', 'c_sgd', 'smd', 'seg', 'adagrad', 'ons', 'sreg', 'sbeg', 'adam', 'adamproj', 'adamp', 'adamax', 'adamtemp', 'adamaxtemp']
-alg_to_run = ['adam', 'adamproj', 'adamp', 'adamtemp', 'adamax', 'adamaxtemp']
-
+alg_to_run = ['gd', 'c_gd', 'sgd', 'c_sgd', 'smd', 'seg', 'adagrad', 'ons',
+              'sreg', 'sbeg', 'adam', 'adamproj', 'adamp', 'adamax', 'adamtemp', 'adamaxtemp']
 
 ############################### Read and prepare data ###############################
 
 mnist_train = pd.read_csv('mnist_train.csv', sep=',', header=None)   # Reading
-train_data = mnist_train.values[:, 1:]                               # Extract data
-train_data = train_data / np.max(train_data)                         # Normalize data
-train_data = np.c_[train_data, np.ones(train_data.shape[0])]         # Add intersept
-train_labels = mnist_train.values[:, 0]                              # Extract labels
-train_labels[np.where(train_labels != 0)] = -1                       # if labels is not 0 => -1 (Convention chosen)
-train_labels[np.where(train_labels == 0)] = 1                        # if label is 0 ==> 1
+# Extract data
+train_data = mnist_train.values[:, 1:]
+# Normalize data
+train_data = train_data / np.max(train_data)
+train_data = np.c_[train_data, np.ones(
+    train_data.shape[0])]         # Add intersept
+# Extract labels
+train_labels = mnist_train.values[:, 0]
+# if labels is not 0 => -1 (Convention chosen)
+train_labels[np.where(train_labels != 0)] = -1
+# if label is 0 ==> 1
+train_labels[np.where(train_labels == 0)] = 1
 
 mnist_test = pd.read_csv('mnist_test.csv', sep=',', header=None)
 test_data = mnist_test.values[:, 1:]
@@ -61,10 +66,12 @@ fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(5, 5))
 
 if 'gd' in alg_to_run:
     model = LinearSVM(m)
-    GDloss, wts = GradientDescent(model, train_data, train_labels, lr, nepoch, lbd, verbose)
+    GDloss, wts = GradientDescent(
+        model, train_data, train_labels, lr, nepoch, lbd, verbose)
     pred_test_labels = model.predict(test_data)
     GDacc = accuracy(test_labels, pred_test_labels)
-    print('After {:3d} epoch, Unconstrained GD algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(nepoch, GDloss[-1], GDacc))
+    print('After {:3d} epoch, Unconstrained GD algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(
+        nepoch, GDloss[-1], GDacc))
     ax[0].plot(np.arange(nepoch), GDloss)
     accuracies = compute_accuracies(wts, test_data, test_labels)
     ax[1].plot(accuracies)
@@ -73,10 +80,12 @@ if 'gd' in alg_to_run:
 
 if 'c_gd' in alg_to_run:
     model = LinearSVM(m)
-    GDprojloss, wts = projected_gd(model, train_data, train_labels, lr, nepoch, lbd, z, verbose)
+    GDprojloss, wts = projected_gd(
+        model, train_data, train_labels, lr, nepoch, lbd, z, verbose)
     pred_test_labels = model.predict(test_data)
     GDacc = accuracy(test_labels, pred_test_labels)
-    print('After {:3d} epoch, constrained GD (radius {:2d} algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(nepoch, z, GDloss[-1], GDacc))
+    print('After {:3d} epoch, constrained GD (radius {:2d} algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(
+        nepoch, z, GDloss[-1], GDacc))
     ax[0].plot(np.arange(nepoch), GDprojloss)
     accuracies = compute_accuracies(wts, test_data, test_labels)
     ax[1].plot(accuracies)
@@ -85,10 +94,12 @@ if 'c_gd' in alg_to_run:
 
 if 'sgd' in alg_to_run:
     model = LinearSVM(m)
-    SGDloss, wts = sgd(model, train_data, train_labels, lr, nepoch, lbd, verbose)
+    SGDloss, wts = sgd(model, train_data, train_labels,
+                       lr, nepoch, lbd, verbose)
     pred_test_labels = model.predict(test_data)
     acc = accuracy(test_labels, pred_test_labels)
-    print('After {:3d} epoch, Unconstrained SGD algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(nepoch, SGDloss[-1], acc))
+    print('After {:3d} epoch, Unconstrained SGD algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(
+        nepoch, SGDloss[-1], acc))
     ax[0].plot(np.arange(nepoch), SGDloss)
     accuracies = compute_accuracies(wts, test_data, test_labels)
     ax[1].plot(accuracies)
@@ -97,70 +108,84 @@ if 'sgd' in alg_to_run:
 
 if 'c_sgd' in alg_to_run:
     model = LinearSVM(m)
-    SGDprojloss, wts = projected_sgd(model, train_data, train_labels, lr, nepoch, lbd, z, verbose)
+    SGDprojloss, wts = projected_sgd(
+        model, train_data, train_labels, lr, nepoch, lbd, z, verbose)
     pred_test_labels = model.predict(test_data)
     acc = accuracy(test_labels, pred_test_labels)
-    print('After {:3d} epoch, constrained SGD algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(nepoch, SGDprojloss[-1], acc))
+    print('After {:3d} epoch, constrained SGD algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(
+        nepoch, SGDprojloss[-1], acc))
     ax[0].plot(np.arange(nepoch), SGDprojloss)
     accuracies = compute_accuracies(wts, test_data, test_labels)
     ax[1].plot(accuracies)
 
 if 'smd' in alg_to_run:
     model = LinearSVM(m)
-    SMDprojloss, wts = smd(model, train_data, train_labels, lr, nepoch, lbd, z, verbose)
+    SMDprojloss, wts = smd(model, train_data, train_labels,
+                           lr, nepoch, lbd, z, verbose)
     pred_test_labels = model.predict(test_data)
     acc = accuracy(test_labels, pred_test_labels)
-    print('After {:3d} epoch, constrained SMD algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(nepoch, SMDprojloss[-1], acc))
+    print('After {:3d} epoch, constrained SMD algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(
+        nepoch, SMDprojloss[-1], acc))
     ax[0].plot(np.arange(nepoch), SMDprojloss)
     accuracies = compute_accuracies(wts, test_data, test_labels)
     ax[1].plot(accuracies)
 
 if 'seg' in alg_to_run:
     model = LinearSVM(m)
-    SEGloss, wts = seg(model, train_data, train_labels, lr, nepoch, lbd, z, verbose)
+    SEGloss, wts = seg(model, train_data, train_labels,
+                       lr, nepoch, lbd, z, verbose)
     pred_test_labels = model.predict(test_data)
     acc = accuracy(test_labels, pred_test_labels)
-    print('After {:3d} epoch, constrained SEG algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(nepoch, SEGloss[-1], acc))
+    print('After {:3d} epoch, constrained SEG algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(
+        nepoch, SEGloss[-1], acc))
     ax[0].plot(np.arange(nepoch), SEGloss)
     accuracies = compute_accuracies(wts, test_data, test_labels)
     ax[1].plot(accuracies)
 
 if 'adagrad' in alg_to_run:
     model = LinearSVM(m)
-    Adagradloss, wts = adagrad(model, train_data, train_labels, lr, nepoch, lbd, z, verbose)
+    Adagradloss, wts = adagrad(
+        model, train_data, train_labels, lr, nepoch, lbd, z, verbose)
     pred_test_labels = model.predict(test_data)
     acc = accuracy(test_labels, pred_test_labels)
-    print('After {:3d} epoch, constrained Adagrad algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(nepoch, Adagradloss[-1], acc))
+    print('After {:3d} epoch, constrained Adagrad algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(
+        nepoch, Adagradloss[-1], acc))
     ax[0].plot(np.arange(nepoch), Adagradloss)
     accuracies = compute_accuracies(wts, test_data, test_labels)
     ax[1].plot(accuracies)
 
 if 'sreg' in alg_to_run:
     model = LinearSVM(m)
-    SREGloss, wts = sreg(model, train_data, train_labels, lr, nepoch, lbd, z, verbose)
+    SREGloss, wts = sreg(model, train_data, train_labels,
+                         lr, nepoch, lbd, z, verbose)
     pred_test_labels = model.predict(test_data)
     acc = accuracy(test_labels, pred_test_labels)
-    print('After {:3d} epoch, constrained SREG algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(nepoch, SREGloss[-1], acc))
+    print('After {:3d} epoch, constrained SREG algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(
+        nepoch, SREGloss[-1], acc))
     ax[0].plot(np.arange(nepoch), SREGloss)
     accuracies = compute_accuracies(wts, test_data, test_labels)
     ax[1].plot(accuracies)
 
 if 'sbeg' in alg_to_run:
     model = LinearSVM(m)
-    SBEGloss, wts = sbeg(model, train_data, train_labels, lr, nepoch, lbd, z, verbose)
+    SBEGloss, wts = sbeg(model, train_data, train_labels,
+                         lr, nepoch, lbd, z, verbose)
     pred_test_labels = model.predict(test_data)
     acc = accuracy(test_labels, pred_test_labels)
-    print('After {:3d} epoch, constrained SBEG algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(nepoch, SBEGloss[-1], acc))
+    print('After {:3d} epoch, constrained SBEG algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(
+        nepoch, SBEGloss[-1], acc))
     ax[0].plot(np.arange(nepoch), SBEGloss)
     accuracies = compute_accuracies(wts, test_data, test_labels)
     ax[1].plot(accuracies)
 
 if 'adam' in alg_to_run:
     model = LinearSVM(m)
-    Adamloss, wts = adam(model, train_data, train_labels, lr, nepoch, lbd, z, [0.9, 0.999], verbose)
+    Adamloss, wts = adam(model, train_data, train_labels,
+                         lr, nepoch, lbd, z, [0.9, 0.999], verbose)
     pred_test_labels = model.predict(test_data)
     acc = accuracy(test_labels, pred_test_labels)
-    print('After {:3d} epoch, adam algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(nepoch, Adamloss[-1], acc))
+    print('After {:3d} epoch, adam algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(
+        nepoch, Adamloss[-1], acc))
     ax[0].plot(np.arange(nepoch), Adamloss)
     accuracies = compute_accuracies(wts, test_data, test_labels)
     ax[1].plot(accuracies)
@@ -168,10 +193,12 @@ if 'adam' in alg_to_run:
 
 if 'adamproj' in alg_to_run:
     model = LinearSVM(m)
-    AdamProjloss, wts = adamproj(model, train_data, train_labels, lr, nepoch, lbd, z, [0.9, 0.999], verbose)
+    AdamProjloss, wts = adamproj(
+        model, train_data, train_labels, lr, nepoch, lbd, z, [0.9, 0.999], verbose)
     pred_test_labels = model.predict(test_data)
     acc = accuracy(test_labels, pred_test_labels)
-    print('After {:3d} epoch, projected adam algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(nepoch, AdamProjloss[-1], acc))
+    print('After {:3d} epoch, projected adam algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(
+        nepoch, AdamProjloss[-1], acc))
     ax[0].plot(np.arange(nepoch), AdamProjloss)
     accuracies = compute_accuracies(wts, test_data, test_labels)
     ax[1].plot(accuracies)
@@ -179,40 +206,48 @@ if 'adamproj' in alg_to_run:
 if 'adamp' in alg_to_run:
     p = 3
     model = LinearSVM(m)
-    AdamPloss, wts = adamP(model, train_data, train_labels, lr, nepoch, lbd, z, [0.9, 0.999], p, verbose)
+    AdamPloss, wts = adamP(model, train_data, train_labels,
+                           lr, nepoch, lbd, z, [0.9, 0.999], p, verbose)
     pred_test_labels = model.predict(test_data)
     acc = accuracy(test_labels, pred_test_labels)
-    print('After {:3d} epoch, adam with norm L{:3d}algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(nepoch, p, AdamPloss[-1], acc))
+    print('After {:3d} epoch, adam with norm L{:3d}algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(
+        nepoch, p, AdamPloss[-1], acc))
     ax[0].plot(np.arange(nepoch), AdamPloss)
     accuracies = compute_accuracies(wts, test_data, test_labels)
     ax[1].plot(accuracies)
 
 if 'adamtemp' in alg_to_run:
     model = LinearSVM(m)
-    AdamTemploss, wts = adamTemporal(model, train_data, train_labels, lr, nepoch, lbd, z, [0.9, 0.999], verbose)
+    AdamTemploss, wts = adamTemporal(
+        model, train_data, train_labels, lr, nepoch, lbd, z, [0.9, 0.999], verbose)
     pred_test_labels = model.predict(test_data)
     acc = accuracy(test_labels, pred_test_labels)
-    print('After {:3d} epoch, adam with temporal averaging algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(nepoch, AdamTemploss[-1], acc))
+    print('After {:3d} epoch, adam with temporal averaging algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(
+        nepoch, AdamTemploss[-1], acc))
     ax[0].plot(np.arange(nepoch), AdamTemploss)
     accuracies = compute_accuracies(wts, test_data, test_labels)
     ax[1].plot(accuracies)
 
 if 'adamax' in alg_to_run:
     model = LinearSVM(m)
-    AdaMaxLoss, wts = adaMax(model, train_data, train_labels, lr, nepoch, lbd, z, [0.9, 0.999], verbose)
+    AdaMaxLoss, wts = adaMax(model, train_data, train_labels, lr, nepoch, lbd, z, [
+                             0.9, 0.999], verbose)
     pred_test_labels = model.predict(test_data)
     acc = accuracy(test_labels, pred_test_labels)
-    print('After {:3d} epoch, AdaMax algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(nepoch, AdaMaxLoss[-1], acc))
+    print('After {:3d} epoch, AdaMax algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(
+        nepoch, AdaMaxLoss[-1], acc))
     ax[0].plot(np.arange(nepoch), AdaMaxLoss)
     accuracies = compute_accuracies(wts, test_data, test_labels)
     ax[1].plot(accuracies)
 
 if 'adamaxtemp' in alg_to_run:
     model = LinearSVM(m)
-    AdaMaxTempLoss, wts = adaMaxTemporal(model, train_data, train_labels, lr, nepoch, lbd, z, [0.9, 0.999], verbose)
+    AdaMaxTempLoss, wts = adaMaxTemporal(
+        model, train_data, train_labels, lr, nepoch, lbd, z, [0.9, 0.999], verbose)
     pred_test_labels = model.predict(test_data)
     acc = accuracy(test_labels, pred_test_labels)
-    print('After {:3d} epoch, AdaMax with temporal averaging algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(nepoch, AdaMaxTempLoss[-1], acc))
+    print('After {:3d} epoch, AdaMax with temporal averaging algorithm has a loss of {:1.6f} and accuracy {:1.6f}'.format(
+        nepoch, AdaMaxTempLoss[-1], acc))
     ax[0].plot(np.arange(nepoch), AdaMaxTempLoss)
     accuracies = compute_accuracies(wts, test_data, test_labels)
     ax[1].plot(accuracies)

0e9f1f2f6c228a6ff3f03663c78b5887030f6f7c
2ailesB <leboudec.lise@gmail.com>
2021-12-26 20:22:52 +0100

    feat/add adam with norm lp

--- Algorithms/Adam.py
+++ Algorithms/Adam.py
@@ -59,6 +59,61 @@ def adam(model, X, y, lr, epoch, l, z=1, betas = [0.9, 0.999], verbose=0):
 
     return losses, np.array(wts)
 
+
+def adamP(model, X, y, lr, epoch, l, z=1, betas = [0.9, 0.999], p=2, verbose=0):
+    """
+        Gradient descent algorithms applied with the CO pb il loss and uses tjhe gradloss function to update parameters
+        :param X: (nxm) data
+        :param y: (n)  labels
+        :param lr: (float) learning rate
+        :param epoch: (int) maximum number of iteration of the algorithm
+        :param l:  (float) regularization parameter (lambda)
+        :param z: (float) radius of the l1-ball
+        :param betas:(1x2) exponential decay rates for the moment estimates
+        :param p: (int) norm to be considered (1 <= p )
+        :param verbose: (int) print epoch results every n epochs
+        """
+
+    n, d = X.shape
+    losses = []
+    wts = [1 / (2*d) * np.zeros(d)]
+    mts = np.zeros(d)
+    mt_1s = np.zeros(d)
+    vts = np.zeros(d)
+    vt_1s = np.zeros(d)
+
+    for i in range(epoch):
+
+        # sample
+        idx = rd.randint(0, n - 1)
+        sample_x = X[idx, :].reshape(1, -1)
+        sample_y = np.array(y[idx])  # need an array for compatibility
+
+        # update the last xt
+        t = i + 1
+
+        mts = betas[0] * mt_1s + (1 - betas[0]) * model.gradLoss(sample_x, sample_y, l)
+        vts = betas[1]**p * vt_1s + (1 - betas[1]**p) * np.abs(model.gradLoss(sample_x, sample_y, l))**p
+        mtchap = mts/(1 - betas[0]**t)
+        vtchap = vts/(1 - betas[1]**t)
+
+        new_wts = wts[-1] - lr * mtchap / (np.power(vtchap, 1/p) + 10e-8)
+        wts.append(new_wts)
+        model.w = new_wts
+
+        # loss
+        current_loss = model.loss(X, y, l)
+        losses.append(current_loss)
+
+        mt_1s = mts
+        vt_1s = vts
+
+        if verbose > 0 and i % verbose == 0:
+            print("Epoch {:3d} : Loss = {:1.4f}".format(i, current_loss))
+
+    return losses, np.array(wts)
+
+
 def adamproj(model, X, y, lr, epoch, l, z=1, betas = [0.9, 0.999], verbose=0):
     """
         Gradient descent algorithms applied with the CO pb il loss and uses tjhe gradloss function to update parameters

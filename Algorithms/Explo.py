fix/add logscale on plots + use mean for computation of accuracy

Algorithms/Explo.py
@@ -53,7 +53,7 @@ def sreg(model, X, y, lr, epoch, l, z=1, verbose=0):
 
     # update wts:
     model.w = np.mean(wts, axis=0)
-    return losses, wts
+    return losses, np.array(wts)
 
 def sbeg(model, X, y, lr, epoch, l, z=1, verbose=0):
     """
@@ -108,4 +108,4 @@ def sbeg(model, X, y, lr, epoch, l, z=1, verbose=0):
 
     # update wts:
     model.w = np.mean(wts, axis=0)
-    return losses, wts
+    return losses, np.array(wts)

Algorithms/RFTL.py
@@ -47,7 +47,7 @@ def smd(model, X, y, lr, epoch, l, z=1, verbose=0):
 
     # update wts:
     model.w = np.mean(wts, axis=0)
-    return losses, wts
+    return losses, np.array(wts)
 
 
 def seg(model, X, y, lr, epoch, l, z=1, verbose=0):
@@ -95,7 +95,7 @@ def seg(model, X, y, lr, epoch, l, z=1, verbose=0):
 
     # update wts:
     model.w = np.mean(wts, axis=0)
-    return losses, wts
+    return losses, np.array(wts)
 
 def adagrad(model, X, y, lr, epoch, l, z=1, verbose=0):
     """
@@ -138,4 +138,4 @@ def adagrad(model, X, y, lr, epoch, l, z=1, verbose=0):
 
     # update wts:
     model.w = np.mean(wts, axis=0)
-    return losses, wts
+    return losses, np.array(wts)

Algorithms/SGD.py
@@ -44,7 +44,7 @@ def sgd(model, X, y, lr, epoch, l, verbose=0):
 
     # update wts:
     model.w = np.mean(wts, axis=0)
-    return losses, wts
+    return losses, np.array(wts)
 
 
 def projected_sgd(model, X, y, lr, epoch, l, z=1, verbose=0):
@@ -88,4 +88,4 @@ def projected_sgd(model, X, y, lr, epoch, l, z=1, verbose=0):
     # update wts:
     model.w = np.mean(wts, axis=0)
     
-    return losses, wts
+    return losses, np.array(wts)